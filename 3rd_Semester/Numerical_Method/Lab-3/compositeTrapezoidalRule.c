f996a85c1abc5dc23ac6574c1c8bc1f55c7d0db8
Suyash Shrestha <sthasuyash11@gmail.com>
2022-11-08 05:07:29 +0545

    fix brackets and spacing

--- 3rd_Semester/Numerical_Method/Lab-3/compositeTrapezoidalRule.c
+++ 3rd_Semester/Numerical_Method/Lab-3/compositeTrapezoidalRule.c
@@ -10,12 +10,14 @@ int main()
 {
 	int n;
 	float a, b, h, ict, sum = 0.0;
+
 	printf("\nGive lower limit of integration: ");
 	scanf("%f", &a);
 	printf("\nGive upper limit of integration: ");
 	scanf("%f", &b);
 	printf("\nGive the segment width h: ");
 	scanf("%f", &h);
+	
 	n = (b - a) / h;
 	for (int i = 1; i <= n; i++)
 	{
@@ -23,6 +25,7 @@ int main()
 	}
 	sum += f(a) + f(b);
 	ict = sum * h / 2;
+	
 	printf("\nIntegration between %f and %f when h=%f is %f.", a, b, h, ict);
 
 	return 0;
--- 3rd_Semester/Numerical_Method/Lab-3/derivatives_centralDifference.c
+++ 3rd_Semester/Numerical_Method/Lab-3/derivatives_centralDifference.c
@@ -3,7 +3,8 @@
 
 float f(float x)
 {
-	return x * x * exp(sqrt(x)) * sin(x);
+	// return x * x * exp(sqrt(x)) * sin(x);
+	return sin(x)+cos(x);
 }
 
 int main()
@@ -14,13 +15,17 @@ int main()
 	{
 		printf("\nEnter the point at which derivatives are required: ");
 		scanf("%f", &a);
+
 		printf("Enter the value of h: ");
 		scanf("%f", &h);
+
 		fd1 = (f(a + h) - f(a - h)) / (2 * h);
 		fd2 = (f(a + h) - 2 * f(a) - f(a - h)) / (h * h);
 		printf("\nThe first and second derivative at x=%f are %f and %f respectively.", a, fd1, fd2);
-		scanf("%c", &q);
 
+		printf("\nDo you want to continue? (y/n): ");
+		fflush(stdin);
+		scanf("%c", &q);
 	} while (q == 'y');
 
 	return 0;
