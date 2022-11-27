08e08fcaa4e65637eda3a41d323954578661c9a8
Suyash Shrestha <sthasuyash11@gmail.com>
2022-11-27 15:49:49 +0545

    update insertion sort

--- 3rd_Semester/DSA/6_Sorting/insertion_sort.cpp
+++ 3rd_Semester/DSA/6_Sorting/insertion_sort.cpp
@@ -1,4 +1,5 @@
-// C++ program for insertion sort
+/* C++ implementation of Insertion Sort */
+
 #include <iostream>
 #include "../array_functions.h"
 using namespace std;
@@ -17,7 +18,7 @@ int main()
     input_array_elements(arr, n);
 
     // before sorting
-    cout << "Unsorted array: ";
+    cout << "Unsorted array: " << endl;
     printArray(arr, n);
 
     // call function to sort array
@@ -33,11 +34,11 @@ int main()
 // insertion sort
 void insertionSort(int arr[], int n)
 {
-    int i, key, j;
-    for (i = 1; i < n; i++)
+    int key;
+    for (size_t i = 1; i < n; i++)
     {
         key = arr[i];
-        j = i - 1;
+        int j = i - 1;
 
         // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
         while (j >= 0 && arr[j] > key)
