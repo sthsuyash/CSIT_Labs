4bed03ecbd6a840e56b7dc1598aeb7e1aa2f4d3f
Suyash Shrestha <sthasuyash11@gmail.com>
2022-11-29 08:21:07 +0545

    add OR:VHDL

---
+++ 3rd_Semester/Computer_Architecture/VHDL_labs/OR.vhd
@@ -0,0 +1,24 @@
+-- VHDL code for AND gate
+
+-- Header file declaration
+
+OR GATE
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY or_gate IS
+PORT (
+input_1 : IN STD_LOGIC;
+input_2 : IN STD_LOGIC;
+or_result : OUT STD_LOGIC
+);
+END or_gate;
+
+-- Dataflow Modelling Style
+-- Architecture declaration
+
+ARCHITECTURE rtl OF or_gate IS
+
+BEGIN
+or_result <= input_1 OR input_2;
+END rtl;
