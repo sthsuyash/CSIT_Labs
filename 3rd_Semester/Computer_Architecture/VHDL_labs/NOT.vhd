bbeaa42ebec2457c70a97919e2ec385d4411c8cc
Suyash Shrestha <sthasuyash11@gmail.com>
2022-11-29 08:21:16 +0545

    add NOT:VHDL

---
+++ 3rd_Semester/Computer_Architecture/VHDL_labs/NOT.vhd
@@ -0,0 +1,24 @@
+-- VHDL code for AND gate
+
+-- Header file declaration
+
+NOT GATE
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+-- Entity declaration
+
+ENTITY not_gate IS
+PORT (
+input_1 : IN STD_LOGIC;
+not_result : OUT STD_LOGIC
+);
+END not_gate;
+
+-- Dataflow Modelling Style
+-- Architecture declaration
+
+ARCHITECTURE rtl OF not_gate IS
+BEGIN
+not_result <= NOT input_1;
+END rtl;
