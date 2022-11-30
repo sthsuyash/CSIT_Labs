d7a287d05c538b202b89ecd1b934ad382f3c5bf8
Suyash Shrestha <sthasuyash11@gmail.com>
2022-11-30 16:48:04 +0545

    update quick sort

--- 3rd_Semester/DSA/6_Sorting/quick_sort.cpp
+++ 3rd_Semester/DSA/6_Sorting/quick_sort.cpp
@@ -31,46 +31,56 @@ int main()
     return 0;
 }
 
-/*
-    This function takes last element as pivot, places
-    the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-    to left of pivot and all greater elements to right
-    of pivot
-*/
-int partition(int arr[], int low, int high)
+int partition(int arr[], int start, int end)
 {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1);     // Index of smaller element and indicates the right position of pivot found so far
+    int pivot = arr[start];
+    int count = 0;
 
-    for (int j = low; j <= high - 1; j++)
+    for (int i = start + 1; i <= end; i++)
     {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot)
+        if (arr[i] <= pivot)
+            count++;
+    }
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+
+        while (arr[i] <= pivot)
         {
-            i++; // increment index of smaller element
-            swap(arr[i], arr[j]);
+            i++;
+        }
+
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
         }
     }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
+    return pivotIndex;
 }
 
-/*
-    The main function that implements QuickSort
-    arr[] --> Array to be sorted,
-    low --> Starting index,
-    high --> Ending index
-*/
-void quickSort(int arr[], int low, int high)
+void quickSort(int arr[], int start, int end)
 {
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now at right place */
-        int pi = partition(arr, low, high);
+    // base case
+    if (start >= end)
+        return;
 
-        // Separately sort elements before partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+    // partitioning the array
+    int p = partition(arr, start, end);
+
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
 }
