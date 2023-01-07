93bf69d2f6937c6538cb8b64f66e16c32eab7d95
Suyash Shrestha <sthasuyash11@gmail.com>
2023-01-07 17:27:10 +0545

    add oop program

---
+++ 2nd_Semester/OOP/Operator_overloading/overloading_binary_operator/overloading_plus_binary_op.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class distances
+{
+private:
+    int feet, inch;
+
+public:
+    distances()
+    {
+    }
+    distances(int f, int i)
+    {
+        feet = f;
+        inch = i;
+    }
+    distances operator+(distances d5)
+    {
+        distances d4;
+        d4.feet = feet + d5.feet;
+        d4.inch = inch + d5.inch;
+        d4.feet = d4.feet + d4.inch / 12;
+        d4.inch = d4.inch % 12;
+        return d4;
+    }
+    void getData()
+    {
+        cout << "Feets=" << feet << endl
+             << "Inches=" << inch << endl;
+    }
+};
+
+int main()
+{
+    distances d1(2, 7), d2(8, 6), d3;
+    d3 = d1 + d2;
+    d3.getData();
+    return 0;
+}
