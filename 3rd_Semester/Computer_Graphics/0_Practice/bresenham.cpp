e88a83b72b11ba5433c35aeb756b357f4db88f48
Suyash Shrestha <sthasuyash11@gmail.com>
2023-01-18 18:08:48 +0545

    add CG:practice bresenham

---
+++ 3rd_Semester/Computer_Graphics/0_Practice/bresenham.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <graphics.h>
+using namespace std;
+
+void drawline(int x0, int y0, int x1, int y1)
+{
+    int dx, dy, p, x, y;
+    int gdriver = DETECT, gmode, error;
+    initgraph(&gdriver, &gmode, "");
+
+    dx = x1 - x0;
+    dy = y1 - y0;
+
+    x = x0;
+    y = y0;
+
+    p = 2 * dy - dx;
+
+    while (x < x1)
+    {
+        if (p >= 0)
+        {
+            putpixel(x, y, 7);
+            y = y + 1;
+            p = p + 2 * dy - 2 * dx;
+        }
+        else
+        {
+            putpixel(x, y, 7);
+            p = p + 2 * dy;
+        }
+        x = x + 1;
+    }
+}
+
+int main()
+{
+    int x0, y0, x1, y1;
+
+    cout << "Enter co-ordinates of first point: ";
+    cin >> x0 >> y0;
+
+    cout << "Enter co-ordinates of second point: ";
+    cin >> x1 >> y1;
+    drawline(x0, y0, x1, y1);
+    getch();
+
+    return 0;
+}
