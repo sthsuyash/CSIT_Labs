a57a7938f697ff64bc4a371ca1acb247aac06f43
Suyash Shrestha <sthasuyash11@gmail.com>
2023-02-11 21:12:32 +0545

    add dfa program

---
+++ 4th_Semester/TOC/DFA/string_contains_011_substring.cpp
@@ -0,0 +1,53 @@
+// cpp program for DFA for the language of string over {0,1} such that each string contains 011 as substring
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;     // string to be checked
+    char state = 0; // initial state (q0)
+
+    cout << "Enter the string: ";
+    cin >> str;
+
+    // loop to check each character of the string for the DFA
+    for (int i = 0; i < str.length(); i++)
+    {
+        // check if the string is over {0,1} or not
+        if (str[i] != '0' && str[i] != '1')
+        {
+            cout << "String not accepted.\nPlease enter a string over {0,1}" << endl;
+            return 0;
+        }
+
+        // dfa transition check
+        if (state == 0 && str[i] == '0')
+            state = 1;
+        else if (state == 0 && str[i] == '1')
+            state = 0;
+        else if (state == 1 && str[i] == '0')
+            state = 1;
+        else if (state == 1 && str[i] == '1')
+            state = 2;
+        else if (state == 2 && str[i] == '0')
+            state = 1;
+        else if (state == 2 && str[i] == '1')
+            state = 3;
+        else if (state == 3 && str[i] == '0')
+            state = 3;
+        else if (state == 3 && str[i] == '1')
+            state = 3;
+    }
+
+    // check if the string is accepted or not,
+    // i.e. if the final state is 3 then string is accepted
+    // else string is not accepted
+    if (state == 3)
+        cout << "String accepted";
+    else
+        cout << "String not accepted";
+
+    return 0;
+}
