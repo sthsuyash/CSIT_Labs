e057d1a1d8e7ea9622e750b60436b3ab5ad8eb22
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-27 07:12:10 +0545

    add dbms file via upload

---
+++ 4th_Semester/DBMS/lab1.sql
@@ -0,0 +1,7 @@
+-- 1. Create a database with your name eg. DBRam
+CREATE DATABASE DBSuyash;
+
+-- 2. Create following table with necessary constraints
+
+-- a. Department table with attributes dept_id(pk, identity), dept_name(not null).
+CREATE TABLE department;
