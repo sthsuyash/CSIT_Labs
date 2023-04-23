730edb85d36d58ddcd137f67fe1297af92efa6d7
Suyash Shrestha <sthasuyash11@gmail.com>
2023-04-23 09:00:57 +0545

    update ending with 01

--- 4th_Semester/TOC/NFA/string_ending_with_01.cpp
+++ 4th_Semester/TOC/NFA/string_ending_with_01.cpp
@@ -1,26 +1,71 @@
-/* WAP to simulate a NFA that accepts string ending with 01 over the alphabet (0, 1) */
-
 #include <iostream>
-#include <string.h>
+#include <vector>
+
 using namespace std;
 
+// Define the NFA as a set of states and transitions
+vector<int> states = {0, 1, 2}; // States are represented by integers (0, 1, 2, ...)
+vector<vector<pair<char, int>>> transitions = {
+    {{'0', 0}, {'1', 0}, {'0', 1}},
+    {{'1', 2}},
+    {{}}}; // Transitions are represented by pairs of characters and states (character, state)
+
+// Define a function to simulate the NFA on a given string
+bool simulate_nfa(string input)
+{
+    // Start at the initial state (state 0)
+    vector<int> current_states = {0};
+
+    // Loop through each character in the input string
+    for (char c : input)
+    {
+        // Find all possible transitions from the current states for the current character
+        vector<int> next_states;
+        for (int state : current_states)
+        {
+            for (auto transition : transitions[state])
+            {
+                if (transition.first == c)
+                {
+                    next_states.push_back(transition.second);
+                }
+            }
+        }
+        // If there are no possible transitions, the input string is not accepted
+        if (next_states.empty())
+        {
+            return false;
+        }
+        // Update the current states to the next states
+        current_states = next_states;
+    }
+    // If the final state is an accepting state, the input string is accepted
+    for (int state : current_states)
+    {
+        if (state == 2)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Define the main function to run the program
 int main()
 {
-    char str[100];
-    int i, l, flag = 0;
+    // Get input from the user
+    string input;
+    cout << "Enter a string to check: ";
+    cin >> input;
 
-    cout << "Enter a string: ";
-    cin >> str;
-    l = strlen(str);
-
-    // Check if string ends with 01
-    if (str[l - 1] == '1' && str[l - 2] == '0')
+    // Simulate the NFA on the input string and output the result
+    if (simulate_nfa(input))
     {
-        cout << "String ends with 01" << endl;
+        cout << "String accepted." << endl;
     }
     else
     {
-        cout << "String does not end with 01" << endl;
+        cout << "String not accepted." << endl;
     }
 
     return 0;
