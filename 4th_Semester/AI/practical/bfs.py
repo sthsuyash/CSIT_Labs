472e97a812097c3dea3646f651b09a647f422178
Suyash Shrestha <sthasuyash11@gmail.com>
2023-07-11 23:45:21 +0545

    add prac files

---
+++ 4th_Semester/AI/practical/bfs.py
@@ -0,0 +1,33 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': ['G', 'H'],
+    'E': ['I'],
+    'F': [],
+    'G': [],
+    'H': [],
+    'I': []
+}
+
+visited = []  # List for visited nodes.
+queue = []  # Initialize a queue
+
+
+def bfs(visited, graph, node):  # function for BFS
+    visited.append(node)
+    queue.append(node)
+
+    while queue:          # Creating loop to visit each node
+        m = queue.pop(0)
+        # print '->' after each node except the last one
+        print(m, end='->' if m != 'I' else '')
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+print("Following is the Breadth-First Search")
+bfs(visited, graph, 'A')
---
+++ 4th_Semester/AI/practical/waterjug.py
@@ -0,0 +1,42 @@
+j1 = 0
+j2 = 0
+x = 4
+y = 3
+
+print("Initial state: (0, 0)")
+print("Capacities: (4, 3)")
+print("Goal state: (2, 0 or any number)")
+
+while j1 != 2:
+    r = int(input("Enter the rule: "))
+    if (r == 1):
+        j1 = x
+    elif (r == 2):
+        j2 = y
+    elif (r == 3):
+        j1 = 0
+    elif (r == 4):
+        j2 = 0
+    elif (r == 5):
+        t = y-j2
+        j2 = y
+        j1 -= t
+        if j1 < 0:
+            j1 = 0
+    elif (r == 6):
+        t = x-j1
+        j1 = x
+        j2 -= t
+        if j2 < 0:
+            j2 = 0
+    elif (r == 7):
+        j2 += j1
+        j1 = 0
+        if j2 > y:
+            j2 = y
+    elif (r == 8):
+        j1 += j2
+        j2 = 0
+        if j1 > x:
+            j1 = x
+    print(j1, j2)
