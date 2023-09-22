d2df920105601ff7e0c693ecaade65fca40435f9
sthsuyash <sthasuyash11@gmail.com>
2023-09-22 20:11:10 +0545

    update in web tech

---
+++ 5th_Semester/Web_Technology/php/login/index.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include 'checkUser.php';
+$obj = new checkLogin();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <?php
+    if ($obj->check()) {
+        ?>
+        <main>
+
+            <div class="container">
+                <div class="title">Welcome
+                    <?php echo $_SESSION['uname']; ?>
+                </div>
+
+                <div class="button">
+                    <a href="logout.php">Logout</a>
+                <?php } else {
+        header("location: login.php?logout=true");
+    } ?>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
--- 5th_Semester/Web_Technology/php/login/saveUser.php
+++ 5th_Semester/Web_Technology/php/login/saveUser.php
@@ -9,6 +9,6 @@ $dob = $_POST["dob"];
 $sql = "INSERT INTO login(uname,email,password,DOB) VALUES ('$uname','$email','$password','$dob')";
 $res = mysqli_query($conn, $sql);
 if (!$res) {
-    die("Operation failed" . mysqli_error($conn));
+    header('location:register.php?success=false');
 }
 header('location:login.php?success=true'); //location is used to go to another page and ? is used to send message to url
