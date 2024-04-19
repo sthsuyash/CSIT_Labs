0e46a9370f09697b5eb082f54bedb29190b07480
Suyash Shrestha <sthasuyash11@gmail.com>
2024-04-19 07:20:41 +0545

    Add Web MVC

---
+++ 6th_Semester/NET_Centric_Computing/Class codes/WebMVC/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models; // to access controllers
+
+/*
+ * Controller is used to handle all the incoming http.request.
+ * It contains group of actions method (such as return view).
+ * which handles the incoming request.
+ * for each controller there should be folder in view and
+ * for each action method there should be file of view.
+ * to access controller from url: tab: domainname/controller/action method
+ * if a class is to be a controller then it should inherit superclass controller in C# controller in C# class
+ */
+namespace WebMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // action method: such method that returns view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            // accessing model
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "John",
+                Gender = "Male",
+                Faculty = "CSIT",
+                Grade = "A"
+            };
+            return View(student);
+        }
+
+        // normal method
+        public string printDetail()
+        {
+            return "Hello CSIT";
+        }
+
+        public string printData(string name, string id)
+        {
+            return $"person having id {id} is {name}";
+        }
+    }
+}
---
+++ 6th_Semester/NET_Centric_Computing/Class codes/WebMVC/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Model is used to store data and logic. 
+ * It is accessible by controller and view to provide necessary response, to use model by controller
+ * It should using project name.models and to use model in view in view it should add @using projectname.models. as model is class it normally conatins properties
+ */
+
+namespace WebMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Faculty { get; set; }
+        public string Grade { get; set; }
+
+    }
+}
